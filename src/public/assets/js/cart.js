7528c00a70090f7be51545fe5619a4197a829e0e
Pedro Tissone <pedrotissone@gmail.com>
2024-06-14 05:29:11 -0300

    implementacion del CRUD en la vista de carrito

--- src/public/assets/js/cart.js
+++ src/public/assets/js/cart.js
@@ -1,10 +1,45 @@
 
-const eliminar = async () => {
-    alert("te voy a eliminar")
+const eliminar = async (cid, pid) => {
+    console.log(cid)
+    console.log(pid)
+
+    let respuesta = await fetch(`/api/carts/${cid}/products/${pid}`, {method: "DELETE"})
+    let productDiv = document.getElementById(`productDiv-${cid}-${pid}`)
+    productDiv.innerHTML = ""
 }
 
-const restar = async () => {
-    alert("voy a restar 1 a la cantidad del producto")
+const restar = async (cid, pid, quantity) => {
+    console.log(cid)
+    console.log(pid)
+    console.log(quantity)
+    if (quantity == 1) {
+        console.log("no se puede restar a 1")
+        return
+    }
+
+    let data = {
+        cantidad: -1
+    }
+    //Al hacer fetch tengo que configurar tambien los headers y el contenido que va a viajar en el body
+    let respuesta = await fetch(`/api/carts/${cid}/products/${pid}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });    
+
+    if(respuesta.ok) {
+        console.log("cantidad actualizada")
+        //Extraigo solo el json que devuelve mi api, que en este caso es la cantidad (sino me devuelve un choclo de cosas)
+        let resultado = await respuesta.json()
+        console.log(resultado)
+        let listQuantity = document.getElementById(`cartQuantity-${cid}-${pid}`)
+        listQuantity.innerHTML = `<li id="cartQuantity-${cid}-${pid}">Cantidad: ${resultado} <button id="cartButtonRestar" onclick="restar('${cid}', '${pid}', '${resultado}')">-</button> <button id="cartButtonSumar" onclick="sumar('${cid}', '${pid}')">+</button> </li>`        
+    } else {
+        console.log("error al actualizar cantidad")
+    }
+    
 }
 
 const sumar = async (cid, pid) => {
@@ -21,8 +56,7 @@ const sumar = async (cid, pid) => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
-    });
-    
+    });    
 
     if(respuesta.ok) {
         console.log("cantidad actualizada")
@@ -30,7 +64,7 @@ const sumar = async (cid, pid) => {
         let resultado = await respuesta.json()
         console.log(resultado)
         let listQuantity = document.getElementById(`cartQuantity-${cid}-${pid}`)
-        listQuantity.innerHTML = `<li id="cartQuantity-${cid}-${pid}">Cantidad: ${resultado} <button id="cartButtonRestar" onclick="restar()">-</button> <button id="cartButtonSumar" onclick="sumar('${cid}', '${pid}')">+</button> </li>`        
+        listQuantity.innerHTML = `<li id="cartQuantity-${cid}-${pid}">Cantidad: ${resultado} <button id="cartButtonRestar" onclick="restar('${cid}', '${pid}', '${resultado}')">-</button> <button id="cartButtonSumar" onclick="sumar('${cid}', '${pid}')">+</button> </li>`        
     } else {
         console.log("error al actualizar cantidad")
     }
@@ -39,7 +73,7 @@ const sumar = async (cid, pid) => {
 const vaciarCarrito = async (cid) => {
     let respuesta = await fetch(`/api/carts/${cid}`, {method: "delete"})
     if (respuesta.ok) {
-        const carrito = document.getElementById("cartUl")
+        const carrito = document.getElementById("cartUl")        
         carrito.innerHTML = ""
     }
 }
